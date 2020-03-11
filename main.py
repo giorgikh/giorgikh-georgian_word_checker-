6b3eb4329caeae1062e99bb8e189cd0dc5662832
giorgikh <giorgikhitarishvili123@gmail.com>
2020-03-11 15:42:50 +0400

    daemata argparse moduli

--- main.py
+++ main.py
@@ -2,17 +2,25 @@
 # -*- coding: utf-8 -*-
 # run command ./main.py > test
 
-
 # import MySQLdb
 import pymysql
+import argparse
+
+
 server = "localhost"
 user_name = "projectUser"
 password = "projectUser!@3"
 db_name = "Georgian_Words"
 query = "select * from correct_words limit 50 "
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--text", type=str, help="teqsti romelic programam unda chaasworos", required=True)
+args = parser.parse_args()
+text = args.text
+print(text)
 
-def connect_mysqldb(query):
+
+def connect_and_fetch_data_db(query):
     try:
         # db = MySQLdb.connect(server, user_name, password, db_name, charset='utf8')
         db = pymysql.connect(server, user_name, password,
@@ -27,7 +35,16 @@ def connect_mysqldb(query):
     print(type(data))
     print(data)
     db.close()
+    return data
+
+
+def check_words(data):
+    for alphabet in data:
+        words = alphabet["word_geo"]
+        for word in words:
+            # მიღებული გვაქვს სიტყვის თითოეული ასო
+            print(word)
 
 
 if __name__ == "__main__":
-    connect_mysqldb(query)
+    check_words(connect_and_fetch_data_db(query))
